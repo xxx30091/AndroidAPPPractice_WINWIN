4b46c80be96bf3496e63cb72421befe5b9e9de61
Cheng-Chong Tang <86001015+xxx30091@users.noreply.github.com>
2022-04-25 01:12:17 +0800

    feat: set up the environment and the basic UI
    1. set up environment
    2. add main page UI & content item UI

---
+++ app/src/main/java/com/arthurtang/androidapppractice_winwin/MainFragment.kt
@@ -0,0 +1,29 @@
+package com.arthurtang.androidapppractice_winwin
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.arthurtang.androidapppractice_winwin.databinding.FragmentMainBinding
+
+class MainFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val binding = FragmentMainBinding.inflate(inflater, container, false)
+
+        
+
+
+
+        return binding.root
+    }
+}
